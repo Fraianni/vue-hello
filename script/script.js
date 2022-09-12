334850cc8791b4d01e740a36c49634a58c2ef7ba
Fraianni <francescoianni1995@gmail.com>
2022-09-12 18:40:08 +0200

    title + image

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#hello-container',
+    data: {
+        title: 'Buongiornissimo',
+    }
+});
+
+const app_2 = new Vue({
+    el: '#img-container',
+    data: {
+        img: 'img/01.jpg'
+    }
+});
